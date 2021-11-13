772151e887b100a962eaa5f38f70d0fa9b79bf99
Matheus Massari <massari.matheus@gmail.com>
2021-11-13 11:33:04 -0300

    desenvolvendo parte de services

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,22 +1,61 @@
-import axios from "axios"
+import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
-class AuthenticateUserService {
-    async execute(code: string) {
-        const url = "https://github.com/login/oauth/access_token"
-
-        const response = await axios.post(url, null, {
-            params: {
-                client_id: process.env.GITHUB_CLIENT_ID,
-                client_secret: process.env.GITHUB_CLIENT_SECRET,
-                code
-            },
-            headers: {
-                "Accept": "application/json"
-            }
-        })
-
-        return response.data;
-    }
+interface IAccessTokenResponse {
+  access_token: string;
+}
+interface IUserResponse {
+    avatar_url: string;
+    login: string;
+    id: number;
+    name: string;
 }
 
-export { AuthenticateUserService }
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+        headers: {
+            authorization: `Bearer ${accessTokenResponse.access_token}`
+        }
+    })
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
+
+    if(!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+    }
+
+    
+    return response.data
+  }
+}
+
+export { AuthenticateUserService };
